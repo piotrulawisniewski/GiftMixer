d0f541e2183ef2c2e5fe9fa6ca9c7e772ba0a159
R32NOR <piotrulawisniewski@gmail.com>
2024-02-12 23:03:08 +0100

    new file added
    main file of  GiftMixer

---
+++ GiftMixerBase.py
@@ -0,0 +1,7 @@
+#GiftMixer project
+#R32NOR|anklebiters
+
+"""GiftMixer is a script that helps to split Christmas wishes to people in group.
+One person buys one present for other one. The reason for build it was to get one bigger gift than few smaller.
+It prevents too much consumption, saves time on pre-christmas rush and helps to spent money wisely. """
+
