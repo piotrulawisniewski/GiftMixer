80296f3eafd63cd6c402ef84f78c4909362c4fe9
R32NOR <piotrulawisniewski@gmail.com>
2024-04-05 13:20:51 +0200

    register and login func added
    first version of register and login- it will be updated (fe. database instead .txt file, salt adding, and few more secure rising funcions

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 #R32NOR|anklebiters
 
 """GiftMixer is a script that helps to split Christmas wishes to people in group.
-One person buys one present for other one. The reason for build it was to get one bigger gift than few smaller.
+One person buys only one present for only one person in the group. The reason for build it was to get one bigger gift than few smaller.
 It prevents too much consumption, saves time on pre-christmas rush and helps to spent money wisely. """
+
+
+
+
 
