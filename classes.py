36fc772c05c9219fbda6bade1956a79d4593c5ee
R32NOR <piotrulawisniewski@gmail.com>
2024-04-09 06:40:30 +0200

    add file to gitignore
    testing gitignore to ensure higher level of security

--- classes.py
+++ classes.py
@@ -1,19 +1,28 @@
+#GiftMixer project
+#R32NOR|anklebiters
+#2024
+
+# file containing class information
+
 import datetime
+
+localTimeStamp = datetime.datetime.now()
+utcTimeStamp = datetime.datetime.now(datetime.timezone.utc)
+localTimeStamp_display = localTimeStamp.strftime("%Y-%m-%d %H:%M:%S")
+utcTimeStamp_display = utcTimeStamp.strftime("%Y-%m-%d %H:%M:%S")
 
 
 
 class User:
 
-    lastID= 100000000 # ID's will be 100000000+
-    def __init__(self , userName , userMail , userPassword):
+    lastID= 100000000   # ID's will be 100000000+
+    def __init__(self , userName, userMail, userPassword):
         self.userID = self.get_next_ID()
         self.userName = userName
         self.userMail = userMail
         self.userPassword = userPassword
         self.localTimeStamp = datetime.datetime.now()
         self.utcTimeStamp = datetime.datetime.now(datetime.timezone.utc)
-
-
 
     @classmethod
     def get_next_ID(cls):
@@ -24,18 +33,12 @@ class User:
 
 
 
+print(localTimeStamp)
+print(utcTimeStamp)
+print(localTimeStamp_display)
+print(utcTimeStamp_display)
 
-    import datetime
 
-    localTimeStamp = datetime.datetime.now()
-    utcTimeStamp = datetime.datetime.now(datetime.timezone.utc)
-    localTimeStamp_display = localTimeStamp.strftime("%Y-%m-%d %H:%M:%S")
-    utcTimeStamp_display = utcTimeStamp.strftime("%Y-%m-%d %H:%M:%S")
-
-    print(localTimeStamp)
-    print(utcTimeStamp)
-    print(localTimeStamp_display)
-    print(utcTimeStamp_display)
 
 
 
